1932c0b28aaad7ead229edecc52025db13a7a564
Arda Çınar <kuzux92@gmail.com>
2017-12-08 22:33:28 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 from distutils.core import setup, Extension
 
-module1 = Extension('qdbc',
+qdbc = Extension('qdbc',
                     sources = ['src/qdbc.c'],
                     include_dirs = ['include'],
                     library_dirs = ['lib'],
                     extra_objects = ['lib/c.o'])
 
-setup (name = 'PackageName',
-       version = '1.0',
-       description = 'This is a demo package',
-       ext_modules = [module1])
-
+setup (name = 'PyQdbc',
+       version = '0.0.1',
+       description = 'kdb/q bindings for python',
+       ext_modules = [qdbc])
